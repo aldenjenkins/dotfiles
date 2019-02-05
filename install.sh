0787b4ba734e248df2cf418839eadeae75ba9077
alden-arch-laptop <alden@aldenjenkins.com>
2019-02-05 11:03:21 -0500

    new configs

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+sudo pip install virtualenvwrapper
 ln -s `pwd`/config $HOME/.config
 ln -s `pwd`/config/vim $HOME/.vim
 ln -s `pwd`/scripts $HOME/.scripts
+ln -s `pwd`/creations $HOME/creations
 ln -s `pwd`/zsh/zshrc $HOME/.zshrc
 mkdir -p ~/Photos/{wallpapers,screenshots}
 mkdir -p ~/Videos/{audio-recordings,Youtube,screen-recordings,memes,nonmemes}
